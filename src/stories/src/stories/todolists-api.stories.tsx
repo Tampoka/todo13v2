13c27d6291c8802c285c6e0f15388fdac430dd41
Tampoka <goal.thanks@gmail.com>
2021-12-13 22:50:16 +0900

    Add CreateTodolist.

--- src/stories/src/stories/todolists-api.stories.tsx
+++ src/stories/src/stories/todolists-api.stories.tsx
@@ -8,9 +8,9 @@ export default {
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        let promise=axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',{withCredentials:true})
-            .then((res)=>{
-               setState(res.data)
+        let promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', {withCredentials: true})
+            .then((res) => {
+                setState(res.data)
             })
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
@@ -22,6 +22,13 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let promise = axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: "What to buy"}, {
+            withCredentials: true,
+            headers: {'API-KEY': '73fdf213-e363-4a31-ad92-85b5d437ac0f'}
+        },)
+            .then((res) => {
+                setState(res.data.item)
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
